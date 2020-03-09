06c80055034314401bf775a54d6ea1c3fccd06a8
mk.thalmann <maik.thalmann@googlemail.com>
2020-03-09 11:07:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Get Latin Square Lists
+
+The script has one main job: read a table-like object (either .csv, .txt, or .xlsx) with each item in all relevant conditions and output several lists (whose number is based on the amount of conditions) based on a Latin Square (see below). Its main intended use case is linguistic experiments design all full-within (conditions are within-items and within-subjects). In addition all single-condition sub-experiments present in the file will be appended to the lists with the critical (multi-condition) items in full. Currently, this only works with one critical sub-experiment; but extending it to overcome this difficulty should be relatively simple.
+
+## Input File
+
+### Critical Items
+
+The following table is a typical (albeit quite short) example of an input file. Note that all items come in four versions representing a condition (read: a permutation) of the same base item. 
+
+| sub_exp | item_number | cond | item                                        |
+| ------- | ----------- | ---- | ------------------------------------------- |
+| 1       | 1           | a    | dass der Peter den Hans mag.                |
+| 1       | 1           | b    | dass den Hans der Peter mag.                |
+| 1       | 1           | c    | dass der Peter sicherlich den Hans mag.     |
+| 1       | 1           | d    | dass den Hans sicherlich der Peter mag.     |
+| 1       | 2           | a    | dass die Frau den Bäcker liebt.             |
+| 1       | 2           | b    | dass den Bäcker die Frau liebt.             |
+| 1       | 2           | c    | dass die Frau sicherlich den Bäcker liebt.  |
+| 1       | 2           | d    | dass den Bäcker sicherlich die Frau liebt.  |
+| 1       | 3           | a    | dass das Auto den Fahrer ärgert.            |
+| 1       | 3           | b    | dass den Fahrer das Auto ärgert.            |
+| 1       | 3           | c    | dass das Auto sicherlich den Fahrer ärgert. |
+| 1       | 3           | d    | dass den Fahrer sicherlich das Auto ärgert. |
+| 1       | 4           | a    | dass der Vater den Hund tritt.              |
+| 1       | 4           | b    | dass den Hund der Vater tritt.              |
+| 1       | 4           | c    | dass der Vater sicherlich den Hund tritt.   |
+| 1       | 4           | d    | dass den Hund sicherlich der Vater tritt.   |
+
+### Fillers
+
+To add fillers (identifed by single-condition items) to the four output lists, the input table should contain them as different sub-experiments. Here's an example with two distinct filler experiments: 
+
+| sub_exp | item_number | cond | item                                          |
+| ------- | ----------- | ---- | --------------------------------------------- |
+| 2       | 1           | a    | Das Essen riechte lecker.                     |
+| 2       | 2           | a    | Das Lied schlagte wie eine Bombe ein.         |
+| 2       | 3           | a    | Das Sicherheitsnetz fangte den Akrobaten auf. |
+| 2       | 4           | a    | Das Wunder geschehte an Weihnachten.          |
+| 3       | 1           | a    | Das Fahrrad tut schnell fahren.               |
+| 3       | 2           | a    | Das Mädchen tut ein neues Kleid tragen.       |
+| 3       | 3           | a    | Das Radio tut laute Musik spielen.            |
+| 3       | 4           | a    | Das Tuch tut im Wind wehen.                   |
+
+## Latin Square
+
+Below is a Latin Square for a 2x2 experiment (where 'a' to 'd' represent the relevant conditions). If the participants are evenly distributed across the four lists (and if enough participants are being tested), you'll have dependent measures by each participant for each item and for each condition of every item (though by different participants). This offers some advantages come statistical analysis. 
+
+|                     | Participant 1 | Participant 2 | Participant 3 | Participant 4 | Participant 5 (same as P1) | ... |
+| ------------------- | ------------- | ------------- | ------------- | ------------- | -------------------------- | --- |
+| Item 1              | a             | b             | c             | d             | a                          | ... |
+| Item 2              | b             | c             | d             | a             | b                          | ... |
+| Item 3              | c             | d             | a             | b             | c                          | ... |
+| Item 4              | d             | a             | b             | c             | d                          | ... |
+| Item 5 (same as I1) | a             | b             | c             | d             | a                          | ... |
+| ...                 | ...           | ...           | ...           | ...           | ...                        | ... |
+
+## Feedback
+
+If you have any comments, feature requests or suggestions, please feel free to send me an [e-mail](mailto:maik.thalmann@gmail.com?subject=[GitHub]%20PyExp).
---
+++ latin_square.py
@@ -0,0 +1,145 @@
+import os
+from itertools import cycle
+
+import pandas as pd
+
+
+def read_multi_ext(file, extension=None):
+    """Read csv, xlsx, and txt files and returns a pandas DataFrame.
+
+    Arguments:
+        file {str} -- File to read in
+
+    Keyword Arguments:
+        extension {str} -- Extension of the file; inferred if None (default: {None})
+
+    Returns:
+        df -- pandas DataFrame
+    """
+    if extension == None:
+        _, extension = os.path.splitext(file)
+    if extension == ".csv":
+        df = pd.read_csv(file, sep=";")
+    elif extension == ".xlsx":
+        df = pd.read_excel(file)
+    elif extension == ".txt":
+        df = pd.read_table(file)
+    return df
+
+
+def save_multi_ext(df, file, extension=None):
+    """Save a pandas DataFrame, depending on extension used in outname or given explicitly.
+
+    Arguments:
+        df {df} -- pandas DataFrame to save
+        file {str} -- Name of the saved file
+
+    Keyword Arguments:
+        extension {str} -- Extension of the file; inferred if None (default: {None})
+    """
+    if extension == None:
+        _, extension = os.path.splitext(file)
+    if extension == ".csv":
+        df.to_csv(file, sep=';', index=False)
+    elif extension == ".xlsx":
+        df.to_excel(file, sheet_name='Sheet1', index=False)
+    elif extension == ".txt":
+        df.to_table(file, index=False)
+
+
+def reorder_columns(df, item_col, sub_exp_col, item_number_col, cond_col):
+    # reorder the most important columns
+    col_order = [item_col, sub_exp_col,
+                 item_number_col, cond_col]
+    # and just add the remaining columns (if any)
+    new_cols = col_order + (df.columns.drop(col_order).tolist())
+    return df[new_cols]
+
+
+def check_permutations(df, item_number_col, cond_col, conditions):
+    # do a cartesian product of item numbers and conditions
+    products = [(item, cond) for item in set(df[item_number_col])
+                for cond in conditions]
+    # check if all such products exist in the dataframe
+    check_list = [((df[item_number_col] == item)
+                   & (df[cond_col] == cond)).any() for item, cond in products]
+    # if they are not all there, raise an error and show which combos are missing
+    if not all(check_list):
+        # get missing combinations
+        missing_combos = ', '.join([''.join(map(str, product)) for product, boolean in zip(
+            products, check_list) if not boolean])
+        raise Exception(
+            f"Not all permutations of items and conditions are present in the dataframe. Missing combinations: {missing_combos}")
+
+
+def to_latin_square(df, outname, sub_exp_col="sub_exp", cond_col="cond", item_col="item", item_number_col="item_number"):
+    """Take a dataframe with all conditions and restructure it with Latin Square. Saves the files.
+
+    Arguments:
+        df {df} -- pandas Dataframe with all conditions for each item
+        outname {str} -- Name for the saved files (uniqueness handled automatically); include extension
+
+    Keyword Arguments:
+        sub_exp_col {str} -- Column containing the subexperiment identifier (default: {"sub_exp"})
+        cond_col {str} -- Column containing the condition identifiers (default: {"cond"})
+        item_col {str} -- Column with the item text (default: {"item"})
+        item_number_col {str} -- Column with the item number (default: {"item_number"})
+
+    Returns:
+        list -- List with all the names of the files that were saved to disk
+    """
+    dfs_critical = []
+    dfs_filler = []
+    name, extension = os.path.splitext(outname)
+    # split the dataframe by the sub experiment value
+    dfs = [pd.DataFrame(x) for _, x in df.groupby(
+        sub_exp_col, as_index=False)]
+    for frame in dfs:
+        # get the unique condition values and sort them
+        conditions = sorted(list(set(frame[cond_col])))
+
+        # check whether all combos of items and conditions are present
+        check_permutations(frame, item_number_col, cond_col, conditions)
+
+        # for filler dfs, just reorder the columns
+        if len(conditions) == 1:
+            frame = reorder_columns(
+                frame, item_col, sub_exp_col, item_number_col, cond_col)
+            # and add them to the correct list
+            dfs_filler.append(frame)
+        # for critical sub experiments generate the appropriate amount of lists
+        else:
+            for k in range(len(conditions)):
+                # order the conditions to match the list being created
+                lat_conditions = conditions[k:] + conditions[:k]
+                # generate (and on subsequent runs reset) the new df with all the columns in the argument df
+                out_df = pd.DataFrame(columns=frame.columns)
+                # look for the appriate rows in the argument df (using the conditions multiple times with 'cycle')
+                out_l = []
+                for item, cond in zip(set(sorted(frame[item_number_col])), cycle(lat_conditions)):
+                    out_l.append(frame[frame.item_number.eq(
+                        item) & frame.cond.eq(cond)])
+                out_df = pd.concat(out_l)
+                # reorder the most important columns
+                out_df = reorder_columns(
+                    out_df, item_col, sub_exp_col, item_number_col, cond_col)
+                dfs_critical.append(out_df)
+
+    # add the fillers to the critical lists
+    for i, df in enumerate(dfs_critical):
+        dfs_critical[i] = pd.concat([df, *dfs_filler])
+
+    for i, df in enumerate(dfs_critical):
+        save_multi_ext(df, f"{name}{i+1}{extension}")
+
+
+if __name__ == "__main__":
+    infile = os.path.join("data", "multi.txt")
+
+    # this one should fail because of the permutation check
+    # infile = os.path.join("data", "multi_exception.txt")
+
+    df = read_multi_ext(infile)
+
+    outfile = os.path.join("latin_squared", "final.csv")
+    to_latin_square(df, outfile)
